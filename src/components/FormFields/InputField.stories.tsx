4615bc8aac9c2ba034cb032b8fad8dce6acfa3ee
Oliver Baek Sedoc <obse@cowi.com>
2025-02-27 23:43:34 +0100

    added storybook

--- src/components/FormFields/InputField.stories.tsx
+++ src/components/FormFields/InputField.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Input } from "./InputField";
-import { Github } from "lucide-react"
 
 const meta: Meta<typeof Input> = {
   title: "Components/FormFields/Input",
